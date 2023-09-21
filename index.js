c35b2cbe01c8a5c997d235be075af25ca0e29664
PiyushWarade007 <128738992+PiyushWarade007@users.noreply.github.com>
2023-09-21 13:06:15 +0530

    add initial Resto-web files

---
+++ index.js
@@ -0,0 +1,36 @@
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+
+//   <script type="text/javascript">
+//   $(".owl-carousel").owlCarousel({
+//     loop: true,
+//     margin: 10,
+//     nav: false,
+//     responsive: {
+//       0: {
+//         items: 1,
+//       },
+//       600: {
+//         items: 2,
+//       },
+//       1000: {
+//         items: 3,
+//       },
+//     },
+//   });
+//   </script>
